0fcbcca2ac56cd28db97d2ecf57b3b2f6a9e0eac
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-09-24 14:08:50 -0400

    Add project files.

---
+++ Contoso.ChatHub/IAgentInitializer.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Agents.AI;
+
+namespace Contoso.ChatHub
+{
+    public interface IAgentInitializer
+    {
+        AIAgent? AIAgent { get; }
+        void Inilitialize(string agentIdentifier);
+    }
+}
